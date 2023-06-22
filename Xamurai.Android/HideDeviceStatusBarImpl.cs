27fd9094fd667c6c658af9fcda067c26d3bb4949
CHIEDU ONWUEMENE <chieduonwuemene@chiedus-mbp-2.lan>
2023-06-22 19:09:19 -0400

    Added HideDeviceStatusBarImpl.cs

---
+++ Xamurai.Android/HideDeviceStatusBarImpl.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Plugin.CurrentActivity;
+using Android.App;
+using Android.Views;
+using Xamurai.Droid;
+
+[assembly: Dependency(typeof(HideDeviceStatusBarImpl))]
+namespace Xamurai.Droid
+{
+    /**
+      * Implement hiding the status bar on Android devices
+     */
+    public class HideDeviceStatusBarImpl : IDeviceStatusBar
+	{
+		public HideDeviceStatusBarImpl()
+		{
+		}
+
+        WindowManagerFlags _originalFlags;
+        
+        public void HideDeviceStatusBar()
+        {
+            Activity activity = CrossCurrentActivity.Current.Activity;
+            var attributes = activity.Window.Attributes;
+            _originalFlags = attributes.Flags;
+            attributes.Flags |= WindowManagerFlags.Fullscreen;
+            activity.Window.Attributes = attributes;
+        }
+
+        public void ShowDeviceStatusBar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
