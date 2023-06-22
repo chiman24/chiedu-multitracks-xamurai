4aebf9c7b77555ba9aae6ee8a858ff46a4bbeb01
CHIEDU ONWUEMENE <chieduonwuemene@chiedus-mbp-2.lan>
2023-06-22 19:02:23 -0400

    Added interface 'IDeviceStatusBar.cs'

---
+++ Xamurai/IDeviceStatusBar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Xamurai
+{
+	//Interface for hiding device status bar
+	public interface IDeviceStatusBar
+	{
+		void ShowDeviceStatusBar();
+		void HideDeviceStatusBar();
+	}
+}
+
