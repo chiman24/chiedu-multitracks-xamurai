1569a6e3c7b92f3843cf14cdc5baac3e1b3d929a
CHIEDU ONWUEMENE <chieduonwuemene@chiedus-mbp-2.lan>
2023-06-22 19:07:50 -0400

    Added HideDeviceStatusBarImpl.cs

---
+++ Xamurai.iOS/HideDeviceStatusBarImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UIKit;
+using Xamurai.iOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HideDeviceStatusBarImpl))]
+namespace Xamurai.iOS
+{
+    /**
+     * Implement hiding status bar on iOS devices.
+     */
+	public class HideDeviceStatusBarImpl : IDeviceStatusBar
+	{
+		public HideDeviceStatusBarImpl()
+		{
+		}
+
+        public void HideDeviceStatusBar()
+        {
+            UIApplication.SharedApplication.StatusBarHidden = true;
+        }
+
+        public void ShowDeviceStatusBar()
+        {
+            UIApplication.SharedApplication.StatusBarHidden = false;
+        }
+    }
+}
+
