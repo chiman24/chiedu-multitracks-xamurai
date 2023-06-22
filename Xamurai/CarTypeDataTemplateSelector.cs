2b30a1dbece7982672d6e96d995c93e4a46a5b42
CHIEDU ONWUEMENE <chieduonwuemene@chiedus-mbp-2.lan>
2023-06-22 19:06:40 -0400

    Added CarTypeDataTemplateSelector

---
+++ Xamurai/CarTypeDataTemplateSelector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Xamurai
+{
+	public class CarTypeDataTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate MercedesTemplate { get; set; }
+		public DataTemplate NonMercedesTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+			return ((Car)item).Make.Equals(CarMake.Mercedes) ? MercedesTemplate : NonMercedesTemplate;
+        }
+    }
+}
+
