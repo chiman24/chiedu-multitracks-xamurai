a2235dad2008a9175f4e097a07b526ce1cdec3b7
CHIEDU ONWUEMENE <chieduonwuemene@chiedus-mbp-2.lan>
2023-06-22 19:20:42 -0400

    Added method: HideDeviceStatusBar()

--- Xamurai/Experiments/ListViewPage.xaml.cs
+++ Xamurai/Experiments/ListViewPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,10 +11,20 @@ namespace Xamurai
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListViewPage : ContentPage
 	{
-		public ListViewPage ()
+        
+		public ListViewPage ()	
 		{
 			BindingContext = new SampleViewModel();
-			InitializeComponent ();
+            InitializeComponent();
+            HideDeviceStatusBar();
 		}
-	}
+
+
+        //Hide device status bar when this page loads.
+        //Implemented in Android and iOS, but currently only works on Android.
+        private void HideDeviceStatusBar()
+        {    
+            DependencyService.Get<IDeviceStatusBar>().HideDeviceStatusBar();
+        }
+    }
 }
